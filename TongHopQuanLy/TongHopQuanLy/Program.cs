794a97a7784d3af50b5801d97ea2cf50a0c7839b
HieuTrong123 <87361973+HieuTrong123@users.noreply.github.com>
2022-03-04 17:30:46 +0700

    tonghop

---
+++ TongHopQuanLy/TongHopQuanLy/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TongHopQuanLy
+{
+    public class SinhVien
+    {
+        public string maSo { get; set; }
+        public string hoTen { get; set; }
+        public int namSinh { get; set; }
+        public string lop { get; set; }
+        public double diem { get; set; }
+        public string xepLoai
+        {
+            get
+            {
+                if (diem >= 9) return "xuat sac";
+                else if (diem < 9 && diem >= 8.5) return "gioi";
+                else if (diem >= 6.5 && diem < 8.5) return "kha";
+                else if (diem >= 4.5 && diem < 6.5) return "trung binh";
+                return "yeu";
+            }
+        }
+        public SinhVien(string maSo, string hoTen, int namSinh, string lop, double diem)
+        {
+            this.maSo = maSo;
+            this.hoTen = hoTen;
+            this.namSinh = namSinh;
+            this.lop = lop;
+            this.diem = diem;
+
+        }
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<SinhVien> ds = new List<SinhVien>();
+
+            QuanLySInhVien ql = new QuanLySInhVien();
+            int luaChon;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\n\t\t=============MENU=============");
+                Console.WriteLine("0,ket thuc chuong trinh");
+                Console.WriteLine("1.tao danh sach");
+                Console.WriteLine("2.xem danh sach");
+                Console.WriteLine("3.nan tem sinh vien co ma so sinh vien cho truoc");
+                Console.WriteLine("4.tim sinh vien co diem trung binh lon nhat");
+                Console.WriteLine("5.sap xep sinh vien theo lop");
+                Console.WriteLine("6.doi mau thong tin cua cac sinh vien co hoc luc kem thanh mau red");
+                Console.WriteLine("7.xoa sinh vien co ma so xho truoc");
+                Console.WriteLine("8.them sinh vien vao vi tri cho truoc");
+                Console.WriteLine("\n\n\t\t=============MENU=============");
+                Console.WriteLine("nhap lua chon cua ban: ");
+                luaChon = int.Parse(Console.ReadLine());
+                switch (luaChon)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        Console.WriteLine("1.tao danh sach");
+                        ql.TaoDS(ds);
+
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("2.xem danh sach");
+                        ql.XemDS(ds);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("3.nan tem sinh vien co ma so sinh vien cho truoc");
+                        string maSo;
+                        Console.WriteLine("nhap ma so sinh vien can nan ten");
+                        maSo = Console.ReadLine();
+                        ql.NanTenSinhVienCoMaSo(maSo, ds);
+                        Console.WriteLine($"xem danh sach sau khi nan ten sinh vien co ma so : {maSo} :");
+                        ql.XemDS(ds);
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("4.tim sinh vien co diem trung binh lon nhat");
+                        ql.TimDiemTBMax(ds);
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        Console.WriteLine("5.sap xep sinh vien theo lop");
+                        ds.Sort(
+                            (a, b) =>
+                        {
+                            if(string.Compare(a.lop, b.lop) > 0)  return 1;
+                            if (string.Compare(a.lop, b.lop) == 0) return 0;
+                            return -1;
+                        });
+                        Console.WriteLine("danh sach sau khi sap xep theo lop la: ");
+                        ql.XemDS(ds);
+                        Console.ReadKey();
+                        break;
+                    case 6:
+                        Console.WriteLine("6.doi mau thong tin cac sinh vien co hoc luc kem thanh mau red");
+                        
+                        Console.ReadKey();
+                        break;
+                    case 7:
+                        Console.WriteLine("7.xoa sinh vien co ma so cho truoc");
+                        Console.ReadKey();
+                        break;
+                    case 8:
+                        Console.WriteLine("8.them sinh vien vao vi tri cho truoc");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("lua chon khong ton tai hay nhap lai!!!");
+                        break;
+                }
+            }
+            //Console.ReadKey();
+        }
+
+    }
+}
---
+++ TongHopQuanLy/TongHopQuanLy/QuanLySInhVien.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TongHopQuanLy
+{
+    internal class QuanLySInhVien
+    {
+        public void TaoDS(List<SinhVien> ds)
+        {
+            try
+            {
+                using (var sr = new StreamReader("Sinhvien.txt"))
+                {
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+
+                        var _temp = temp.Split(',');
+                        var k = new SinhVien(_temp[0], _temp[1]
+                            , int.Parse(_temp[2]), _temp[3], double.Parse(_temp[4]));
+                        ds.Add(k);
+
+                    }
+
+
+
+                }
+                Console.WriteLine("thanh cong!!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("tao danh sach that bai!!!");
+            }
+
+        }
+        public void XuatDongKe(char x)
+        {
+            Console.WriteLine();
+            Console.Write(':');
+            for (int i = 0; i < 69; i++)
+            {
+                Console.Write(x);
+            }
+            Console.Write(':');
+            Console.WriteLine();
+        }
+        public void XuatTieuDe()
+        {
+            XuatDongKe('=');
+            Console.WriteLine(':' + "ma so".PadRight(10) + ':' + "ho ten".PadRight(30) + ':' + "nam sinh".PadRight(8)
+                + ':' + "lop".PadRight(6) + ':' + "diem TB".PadRight(6) + ':' + "xep loai".PadRight(8));
+            XuatDongKe('=');
+        }
+        public void Xuat1SV(SinhVien sv)
+        {
+            Console.WriteLine(':' + sv.maSo.PadRight(10) + ':' + sv.hoTen.PadRight(30) + ':' + sv.namSinh.ToString().PadRight(8)
+                + ':' + sv.lop.PadRight(6) + ':' + sv.diem.ToString().PadRight(6) + ':' + sv.xepLoai.PadRight(8));
+        }
+        public void XemDS(List<SinhVien> ds)
+        {
+            XuatTieuDe();
+            foreach (SinhVien s in ds)
+            {
+                Xuat1SV(s);
+            }
+            XuatDongKe('=');
+        }
+        public string NanTen1SV(SinhVien sv)
+        {
+            string m;
+            string ketQua = "";
+            var temp = sv.hoTen.Trim().Split(' ');
+            foreach (var t in temp)
+            {
+                if (t != "")
+                {
+                    try
+                    {
+                        var l = t.Trim().ToUpper();
+                        var k = l.Substring(1, l.Length - 1);
+                        m = l.Replace(k, k.ToLower());
+                        ketQua += m + ' ';
+
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+
+            }
+            return ketQua;
+        }
+        public void NanTenSinhVienCoMaSo(string maSo, List<SinhVien> ds)
+        {
+            foreach (SinhVien s in ds)
+            {
+                if (string.Compare(s.maSo, maSo) == 0)
+                {
+                    s.hoTen = NanTen1SV(s);
+                }
+            }
+        }
+        public void TimDiemTBMax(List<SinhVien> ds)
+        {
+            double max = ds[0].diem;
+            foreach (SinhVien s in ds)
+            {
+                if (s.diem > max)
+                {
+                    max = s.diem;
+                }
+
+            }
+            var temp = ds.FindAll(
+                (e) =>
+                {
+                    return (e.diem == max);
+                });
+            Console.WriteLine($"danh sach hoc sinh co diem max {max} la:");
+            XuatTieuDe();
+            foreach (SinhVien s in temp)
+            {
+                Xuat1SV(s);
+            }
+            XuatDongKe('=');
+        }
+    }
+
+}
